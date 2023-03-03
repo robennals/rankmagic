8d0cdd1507d5659013ba6440e62da09b6d4cda0f
robennals <rob@ennals.org>
2023-03-03 09:14:41 -0800

    Split fetchJson into own function

--- client/data.js
+++ client/data.js
@@ -1,5 +1,13 @@
 
 
+export async function fetchJson(url) {
+    const response = await fetch(url, {
+        headers: {'Content-Type': 'application/json'}
+    });
+    const data = await response.json();
+    return data;
+}
+
 export async function getTootsAsync() {
     const actionUrl = 'https://mastodon.social/api/v1/timelines/public?limit=10';
     const response = await fetch(actionUrl, {
@@ -9,4 +17,13 @@ export async function getTootsAsync() {
     console.log('got response JSON', data);
     return data;
 }
+
+export async function getFollowingAsync({id}) {
+    return fetchJson('https://mastodon.social/api/v1/accounts/' + id + '/following');
+}
+
+const getId = 'https://mastodon.social/api/v1/accounts/lookup?acct=@elipariser'
+export const eliId = 109236433339564533
+const getFollowing = 'https://mastodon.social/api/v1/accounts/109236433339564533/following'
+
 
